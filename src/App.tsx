3f26516ed9f89b9cf74878db45ad83b5c31e7420
Tatsuhiko Arai <tatsu@tatsu.com>
2025-08-28 11:02:24 +0900

    Add Amplify configuration to main.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+import { Amplify } from "aws-amplify";
 
 function App() {
   const [count, setCount] = useState(0);
-
+  useEffect(() => {
+    console.log("Amplify.configure:");
+    console.log(Amplify.getConfig());
+  }, []);
   return (
     <>
       <div>
--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,10 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
+import outputs from "../amplify_outputs.json";
+import { Amplify } from "aws-amplify";
+
+Amplify.configure(outputs);
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
